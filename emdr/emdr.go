28ac8fd056c9ecdd1abfe1831e70e25bd220cdea
Onno Lissenberg <orlissenberg@gmail.com>
2015-02-19 00:28:46 +0100

    EmdrWriter

--- emdr/emdr.go
+++ emdr/emdr.go
@@ -7,6 +7,11 @@ import (
 	"io/ioutil"
 	"compress/zlib"
 	zmq "github.com/pebbe/zmq2"
+	"time"
+	"strconv"
+	"encoding/json"
+	"os"
+	elastic "github.com/mattbaird/elastigo/lib"
 )
 
 type EmdrClient struct {
@@ -21,16 +26,122 @@ func NewEmdr(address string) (client *EmdrClient, err error) {
 	return
 }
 
-func (client *EmdrClient) Start() {
-	for count := 0; count < 10; {
+type EmdrWriter interface {
+	Write(message []byte) (err error)
+	WriteOrder(message []byte) (err error)
+	WriteHistory(message []byte) (err error)
+}
+
+type ElasticEmdrWriter struct {}
+
+type EmdrMessage struct {
+	ResultType string "resultType"
+}
+
+func (writer *ElasticEmdrWriter) Write(message []byte) (err error) {
+	var v EmdrMessage
+	json.Unmarshal(message, &v)
+
+	switch v.ResultType {
+	case "orders":
+		err = writer.WriteOrder(message)
+	case "history":
+		err = writer.WriteHistory(message)
+	}
+
+	return
+}
+
+func UnixTimeStampString() string {
+	return strconv.FormatInt(time.Now().Unix(), 10)
+}
+
+type EmdrOrderRowSet struct {
+	TypeId      int64 "typeID"
+	//	TypeName    string
+	RegionID    int64 "regionID"
+	GeneratedAt string "generatedAt"
+	Rows        [][]interface{} "rows" // Format not Elasticsearch friendly
+	DataRows    []map[string]interface{}
+}
+
+type EmdrOrderMessage struct {
+	ResultType  string "resultType"
+	UploadKeys  []map[string]string "uploadKeys"
+	Generator   map[string]string "generator"
+	CurrentTime string "currentTime"
+	Version     string "version"
+	RowSets     []EmdrOrderRowSet "rowsets"
+	Columns     []string "columns"
+}
+
+func (writer *ElasticEmdrWriter) WriteOrder(message []byte) (err error) {
+	c := elastic.NewConn()
+	c.Hosts = []string{"localhost"}
+	fmt.Println("Order: " + UnixTimeStampString())
+
+	order := new(EmdrOrderMessage)
+	json.Unmarshal(message, order)
+
+	// Rewrite the data sets to a key:value format.
+	// Loop the sets.
+	for setIndex, setValue := range order.RowSets {
+		order.RowSets[setIndex].DataRows = make([]map[string]interface{}, len(order.RowSets[setIndex].Rows))
+
+		// Loop the rows.
+		for rowIndex, rowValue := range setValue.Rows {
+			// Create a new mapping.
+			mapping := make(map[string]interface{})
+			for index, name := range order.Columns {
+				mapping[name] = rowValue[index]
+			}
+			order.RowSets[setIndex].DataRows[rowIndex] = mapping
+		}
+
+		// Remove the old data
+		order.RowSets[setIndex].Rows = nil
+	}
+
+	_, err = c.Index("eve", "order", "", nil, order)
+
+	if err != nil {
+		// Dump data to file system on error for inspection.
+		ioutil.WriteFile("error_order_"+strconv.FormatInt(time.Now().Unix(), 10)+".json", message, 0644)
+	}
+
+	return
+}
+
+func (writer *ElasticEmdrWriter) WriteHistory(message []byte) (err error) {
+	c := elastic.NewConn()
+	c.Hosts = []string{"localhost"}
+	fmt.Println("History: " + UnixTimeStampString())
+	_, err = c.Index("eve", "history", "", nil, string(message))
+
+	return
+}
+
+func (client *EmdrClient) Start(writer EmdrWriter) {
+	errCount := 0
+
+	for {
 		msg, err := client.sub.Recv(zmq.SNDMORE)
 
+		if err == nil {
+			var output []byte
+			output, err = ZlibDecode(msg)
+
+			if err == nil {
+				err = writer.Write(output)
+			}
+		}
+
 		if err != nil {
-			fmt.Println(err)
-			count++;
-		} else {
-			output, _ := ZlibDecode(msg)
-			fmt.Println(string(output))
+			if (errCount < 10) {
+				errCount++
+			} else {
+				os.Exit(1)
+			}
 		}
 	}
 }
