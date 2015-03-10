4b65f6ce30e7b4954e787b0aa8f358f32ee2e137
Onno Lissenberg <orlissenberg@gmail.com>
2015-03-10 21:42:15 +0100

    Bug fix.

--- fix_centos7.sh
+++ fix_centos7.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-if [ $1 == "undo" ]
+if [ $# -gt 0  ]
 then
-    sed -i -- 's/zmq3/zmq2/g' setup.sh
-    sed -i -- 's/zmq3/zmq2/g' ./src/emdr/emdr.go
-    mv ./src/sqlserver/transfer.tmp ./src/sqlserver/transfer.go
+    if [ $1 == "undo" ]
+    then
+        sed -i -- 's/zmq3/zmq2/g' setup.sh
+        sed -i -- 's/zmq3/zmq2/g' ./src/emdr/emdr.go
+        mv ./src/sqlserver/transfer.tmp ./src/sqlserver/transfer.go
+    fi
 else
     sed -i -- 's/zmq2/zmq3/g' setup.sh
     sed -i -- 's/zmq2/zmq3/g' ./src/emdr/emdr.go
